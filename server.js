5691f9e0ee3ccc631bab1250ed70f9c0930633bb
SAIKAT24072002 <saikatkhamrai0702@gmail.com>
2026-07-05 00:51:31 +0530

    first commit

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(notFound);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
